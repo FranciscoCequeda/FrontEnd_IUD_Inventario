c289b9ca083e8241346dfc1dfde166c12d178025
Francisco Cequeda <edwardo_99@hotmail.com>
2022-10-10 17:39:36 -0500

    Creacion de estructura de carpetas y edicion de archivos de react

---
+++ src/components/tiposequipos/TipoEquipos.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TipoEquipos() {
+  return (
+    <div>TipoEquipos</div>
+  )
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from "../components/ui/NavBar";
+
+export default function AppRouter() {
+  return (
+    <div>
+    <NavBar/>
+
+    </div>
+  )
+}
